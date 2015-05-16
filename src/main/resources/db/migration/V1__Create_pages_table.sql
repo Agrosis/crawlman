b322ad4fd463481861cc6d78fbd586fb16429675
Muhtasim Ahsan <agro@jantox.com>
2015-05-16 17:19:47 -0500

    added migrations.

---
+++ src/main/resources/db/migration/V1__Create_pages_table.sql
@@ -0,0 +1,6 @@
+
+CREATE TABLE `pages` (
+  `id` bigint(11) NOT NULL,
+  `title` text NOT NULL,
+  `content_digest` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ;
---
+++ src/main/resources/db/migration/V2__Create_links_table.sql
@@ -0,0 +1,5 @@
+
+CREATE TABLE `links` (
+  `source` bigint(11) NOT NULL,
+  `target` bigint(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ;
